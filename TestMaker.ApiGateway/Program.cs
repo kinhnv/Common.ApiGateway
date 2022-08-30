13f4a33541cbdd1f7388d9b4fd2ad30a9cbd29c9
Nguyen Van Kinh <kinhnv1801@gmail.com>
2022-08-31 01:25:07 +0700

    add core

--- TestMaker.ApiGateway/Program.cs
+++ TestMaker.ApiGateway/Program.cs
@@ -4,7 +4,18 @@ using Ocelot.ClientGateway;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddClientGatewayOcelot();
 
+// Add Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll",
+        builder => builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 app.UseOcelot().Wait();
 
